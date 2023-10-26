67e65ee8a6effffa1e4b32679b41d06214c35f23
ramocreative <138344638+ramocreative@users.noreply.github.com>
2023-10-26 21:27:56 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Navigation
+
+const hamburgerButton = document.querySelector('.hamburger-button');
+const navigation = document.querySelector('.navigation');
+
+hamburgerButton.addEventListener('click', () => {
+    navigation.hasAttribute('data-visible')
+        ? hamburgerButton.setAttribute('aria-expanded', false)
+        : hamburgerButton.setAttribute('aria-expanded', true);
+    navigation.toggleAttribute('data-visible');
+});
