2842f5d62cf3e5f0a49a18eb8e9371c128dc3fc1
ramocreative <138344638+ramocreative@users.noreply.github.com>
2023-10-28 11:01:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,27 @@ hamburgerButton.addEventListener('click', () => {
         : hamburgerButton.setAttribute('aria-expanded', true);
     navigation.toggleAttribute('data-visible');
 });
+
+// Scroll up
+
+let calcScrollValue = () => {
+    let scrollProgress = document.getElementById("progress");
+    let progressValue = document.getElementById("progress-value");
+    let pos = document.documentElement.scrollTop;
+    let calcHeight =
+        document.documentElement.scrollHeight -
+        document.documentElement.clientHeight;
+    let scrollValue = Math.round((pos * 100) / calcHeight);
+    if (pos > 100) {
+        scrollProgress.style.display = "flex";
+    } else {
+        scrollProgress.style.display = "none";
+    }
+    scrollProgress.addEventListener("click", () => {
+        document.documentElement.scrollTop = 0;
+    });
+    scrollProgress.style.background = `conic-gradient(#31e981 ${scrollValue}%, #fcfcfc ${scrollValue}%)`;
+    };
+
+    window.onscroll = calcScrollValue;
+    window.onload = calcScrollValue;
